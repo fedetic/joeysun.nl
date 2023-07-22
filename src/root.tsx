90510adc06fe2a7cce56705fcd447191dc31dd88
Joey <joeysun95@gmail.com>
2023-07-22 14:16:01 +0200

    Update root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -18,15 +18,14 @@ export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - Bare</Title>
+        <Title>Joey Sun - Bare</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Body>
         <Suspense>
           <ErrorBoundary>
-            <A href="/">Index</A>
-            <A href="/about">About</A>
+            <A href="/">Home</A>
             <Routes>
               <FileRoutes />
             </Routes>
